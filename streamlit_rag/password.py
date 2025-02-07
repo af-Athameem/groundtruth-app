2a4e33ecef82cec167b0541862542edb28d3d2ce
af-Athameem <athameem@ampliforce.com>
2025-02-07 15:51:06 -0500

    Delete streamlit_rag directory

--- streamlit_rag/password.py
+++
@@ -1,9 +0,0 @@
-import streamlit_authenticator as stauth
-
-# List of plain-text passwords
-passwords = ['', '']
-
-# Hash the passwords
-hashed_passwords = stauth.Hasher(passwords).generate()
-
-print(hashed_passwords)
